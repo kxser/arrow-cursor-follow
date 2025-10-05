2b287f9f7cb918c860d8c71410cb86afe731a1d0
mainframe <kaiser@mainframe.local>
2025-10-05 17:04:19 +0300

    ghpages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/arrow-cursor-follow/',
-    buildAssetsDir: '/_nuxt/'
+    baseURL: process.env.NUXT_APP_BASE_URL || '/'
   },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
-
+  modules: ['@nuxt/ui'],
+  nitro: {
+    preset: 'github_pages'
+  }
 })
