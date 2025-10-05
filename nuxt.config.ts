b71195362469869888d03c264ed8e8549d416730
mainframe <kaiser@mainframe.local>
2025-10-05 16:53:23 +0300

    fixed nuxtconfigts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/arrow-cursor-follow/',
+    buildAssetsDir: '/_nuxt/'
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui']
